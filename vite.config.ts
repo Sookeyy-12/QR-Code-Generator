9cb26148de45b3a7b6e07da37bcc51fa8b09cae2
Sookeyy-12 <thatonesuket@gmail.com>
2024-10-23 00:03:51 +0530

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "QR-Code-Generator",
+  base: "/QR-Code-Generator/",
   plugins: [react()],
 })
